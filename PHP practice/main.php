36bcc5f07abba5342e2b08ef42fbaf0daf786f81
sari7happy <sari7happy@gmail.com>
2022-09-21 12:50:29 +0900

    ”for文練習”

--- PHP practice/main.php
+++ PHP practice/main.php
@@ -1,13 +1,12 @@
-<select name="age">
+
 <?php  
 // while文は繰り返しの処理、年齢入力フォーム
-$i =1;
-while($i <=50){
-    echo"<option>".$i."歳</option>";
-    $i++;
+for($year =1989;$year<=2015; $year++){
+    $heisei =$year-1988;
 
+    echo"西暦" .$year."年は平成".$heisei."年です。\n";
 
 }
+
 ?>
 
-</select>
