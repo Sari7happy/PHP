941c71324166186294517815d06aa7b51b44da59
sari7happy <sari7happy@gmail.com>
2022-09-21 12:21:02 +0900

    ”年齢のプルダウン追加”

--- PHP practice/main.php
+++ PHP practice/main.php
@@ -1,12 +1,13 @@
+<select name="age">
 <?php  
-// while文は繰り返しの処理
-$i = 2;
-while($i <= 50){
-    echo $i."\n";
+// while文は繰り返しの処理、年齢入力フォーム
+$i =1;
+while($i <=50){
+    echo"<option>".$i."歳</option>";
     $i++;
-    // ++でプラスしていく
+
+
 }
-
-
 ?>
 
+</select>
